7394341dfd9f99158ffa1f567aaff6f2219428fa
Mikhail Khapilov <A231064MK@rvt.lv>
2026-02-25 10:14:38 +0200

    updated users table structure

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -21,6 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'avatar',
+        'bio',
+        'isPrivate',
     ];
 
     /**
@@ -43,6 +46,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'isPrivate' => 'boolean',
         ];
     }
 }
--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,10 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->string('avatar')->nullable();
+            $table->string('bio')->nullable();
+            $table->boolean("isPrivate")->default(false);
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
